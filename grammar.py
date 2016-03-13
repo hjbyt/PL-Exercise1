1a715c539dddaa7baa1a622bebf9dace6ec17767
hjbyt <sopu00@gmail.com>
2016-03-13 17:24:30 +0200

    implemented grammar_json_4a

--- grammar.py
+++ grammar.py
@@ -199,9 +199,14 @@ def analyze_grammar(grammar):
 
 
 grammar_json_4a = [
-    #
-    # --- FILL IN HERE IN QUESTION 4.a ---
-    #
+    (obj, (LB, RB)),
+    (obj, (LB, members, RB)),
+    (members, (keyvalue,)),
+    (members, (members, COMMA, members)),
+    (keyvalue, (STRING, COLON, value)),
+    (value, (STRING,)),
+    (value, (INT,)),
+    (value, (obj,)),
 ]
 
 grammar_json_4b = [
@@ -231,8 +236,8 @@ def main():
     #
     # --- UNCOMMENT THE FOLLOWING LINES AS YOU PROCEED ---
     #
-    # analyze_grammar(grammar_json_4a)
-    # print
+    analyze_grammar(grammar_json_4a)
+    print
     # analyze_grammar(grammar_json_4b)
     # print
     # analyze_grammar(grammar_json_4c)
